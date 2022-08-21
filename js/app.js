1f75d80e11c7c452315fbfed39213491f9d46f30
Salah Eddine Soussou <88331708+SalahSoussou@users.noreply.github.com>
2022-08-21 19:08:52 +0100

    ok

--- js/app.js
+++ js/app.js
@@ -3,6 +3,5 @@ links.onclick = () => {
   togel = !togel;
   menuIcon.classList.add(togel ? "fa-times" : "fa-bars");
   menuIcon.classList.remove(togel ? "fa-bars" : "fa-times");
-  //   navUl.style.right = togel ? "0px" : "-500px";
   navUl.style.display = togel ? "block" : "none";
 };
